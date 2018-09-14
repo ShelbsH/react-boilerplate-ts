0e5c8789f1d6dab38c04e9684ae535793c95c895
shurnster <slainuponmyshield@gmail.com>
2018-09-13 23:04:58 -0700

    fixed issues with prod and dev env, fixed loaders, added more dependencies

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 //=============================================
 // Express
-const express = require("express");
+const express = require('express');
 const app = express();
 
 //=============================================
 // Regular dependency tools for express
-const path = require("path");
-const morgan = require("morgan");
+const path = require('path');
+const morgan = require('morgan');
 
 //=============================================
 // Port number for when the application is running
@@ -14,36 +14,38 @@ const PORT = 3000;
 
 //=============================================
 // Set the identifier to set the file path directory
-const filePath = path.resolve(__dirname, "dist/");
+const filePath = path.resolve(__dirname, 'dist/');
 
 //=============================================
 // Standard tools to be used for the webpack
 // middleware server
-const webpackDevMiddleware = require("webpack-dev-middleware");
-const webpackConfig = require("../webpack.config.js");
-const webpack = require("webpack");
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackConfig = require('../webpack.config.js');
+const webpack = require('webpack');
 const compiler = webpack(webpackConfig);
 
-app.use(
-  webpackDevMiddleware(compiler, {
-    filePath: "/",
-    contentBase: filePath,
-    hot: true
-  })
-);
+if (process.env.NODE_ENV === 'development') {
+  app.use(
+    webpackDevMiddleware(compiler, {
+      filePath: '/',
+      contentBase: filePath,
+      hot: true
+    })
+  );
 
-app.use(require("webpack-hot-middleware")(compiler));
+  app.use(require('webpack-hot-middleware')(compiler));
+}
 
 //=============================================
 // Morgan Logger
-app.use(morgan("combined"));
+app.use(morgan('combined'));
 
 //Sets the file path directory to load up scripts and styles from the HTML file.
 app.use(express.static(filePath));
 
 //Send the HTML file on root index GET request
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../views/index.html"));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../views/index.html'));
 });
 
 //Send image files based on GET request.
@@ -52,4 +54,4 @@ app.get(/^\/.*\.(png|jpg)$/, (req, res) => {
 });
 
 //Run the application on PORT, 3000.
-app.listen(PORT, () => console.log("The port, " + PORT + " is running"));
+app.listen(PORT, () => console.log('The port, ' + PORT + ' is running'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,71 @@
-const webpack = require("webpack");
-const path = require("path");
+const webpack = require('webpack');
+const path = require('path');
 
 //Plugins
-const prettier = require("prettier-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const prettier = require('prettier-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const isProduction = process.env.NODE_ENV === "production";
+const isProduction = process.env.NODE_ENV === 'production';
+
+let devEntry = [
+  './app/index.tsx',
+  'webpack/hot/only-dev-server',
+  'webpack-hot-middleware/client?path=http://localhost:3000/__webpack_hmr&reload=true'
+];
+let prodEntry = ['./app/index.tsx'];
 
 module.exports = {
-  entry: [
-    "./app/index.tsx",
-    "webpack/hot/only-dev-server",
-    "webpack-hot-middleware/client?path=http://localhost:3000/__webpack_hmr&reload=true"
-  ],
+  entry: isProduction ? prodEntry : devEntry,
   output: {
-    filename: "app.bundle.js",
-    path: path.resolve(__dirname, "dist")
+    filename: 'app.bundle.js',
+    path: path.resolve(__dirname, 'dist')
   },
-  devtool: "eval",
-  mode: !isProduction ? 'development' : 'production',
+  devtool: 'eval',
+  mode: isProduction ? 'production' : 'development',
   module: {
     rules: [
       {
         test: /\.tsx?$/,
         use: [
           !isProduction && {
-            loader: "babel-loader",
+            loader: 'babel-loader',
             options: {
-              plugins: ["react-hot-loader/babel"]
+              plugins: ['react-hot-loader/babel']
             }
           },
-          "awesome-typescript-loader"
-        ]
+          'awesome-typescript-loader'
+        ].filter(Boolean)
       },
       {
         test: /(\.scss|\.css)/,
-        use: ["style-loader", "css-loader", "sass-loader"]
+        use: [
+          !isProduction ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
       },
       {
         test: /(\.woff|\.ttf|\.svg|\.eot|\.gif)/,
-        use: "url-loader"
+        use: 'url-loader'
       }
     ]
   },
+  optimization: {
+    namedChunks: true
+  },
   resolve: {
     alias: {
-      styles: path.resolve(__dirname, "app/styles/"),
-      app: path.resolve(__dirname, "app/")
+      styles: path.resolve(__dirname, 'app/styles/'),
+      app: path.resolve(__dirname, 'app/')
     },
-    extensions: [".ts", ".tsx", ".js", ".jsx"]
+    extensions: ['.ts', '.tsx', '.js', '.jsx']
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new webpack.SourceMapDevToolPlugin({
-      filename: "[name].js.map"
+      filename: '[name].js.map'
     }),
     new prettier({
       bracketSpacing: true
@@ -62,6 +73,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: 'views/index.html'
     }),
-    new CleanWebpackPlugin(['dist'])
+    new CleanWebpackPlugin(['dist']),
+    new MiniCssExtractPlugin()
   ]
 };
